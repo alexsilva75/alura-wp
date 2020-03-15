2b95e7b4fef15ae801fc6917c4b0cefe8b72b96e
Alexsandro Silva <alexsandro.ssantos75@gmail.com>
2020-03-15 11:27:27 -0300

    Versão final do projeto!

--- wp-content/themes/malura/header.php
+++ wp-content/themes/malura/header.php
@@ -1,21 +1,29 @@
-<?php
-/**
- * Theme: Malura
- * 
- * The Header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="content">
- *
- * @package malura
- */
-?><!DOCTYPE html>
+<!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <?php $home = get_template_directory_uri(); ?>
-        <link rel="stylesheet" type="text/css" href="<?= $home;?>/reset.css">
-        <link rel="stylesheet" type="text/css" href="<?= $home;?>/style.css">
-        <title>Malura - Formação WordPress</title>
-        <?php wp_head(); ?>
-    </head>
-    <body>
+<head>
+	<?php $home = get_template_directory_uri(); ?>
+	<meta charset="utf-8">
+	<title>
+		<?php get_titulo(); ?>
+	</title>
+
+	<link rel="stylesheet" href="<?= $home ?>/assets/css/reset.css">
+	<link rel="stylesheet" href="<?= $home; ?>/assets/css/comum.css">
+	<link rel="stylesheet" href="<?= $home; ?>/assets/css/header.css">
+
+	<link rel="stylesheet" href="<?= $home; ?>/assets/css/<?= $css_escolhido; ?>.css">
+
+
+	<?php	wp_head(); ?>
+</head>
+<body>
+
+<header>
+	<div class="container">
+		<?php
+			$args = array( 'theme_location' => 'header-menu');
+			wp_nav_menu( $args ); 
+		?>
+	</div>
+
+</header>
--- wp-content/themes/malura/single.php
+++ wp-content/themes/malura/single.php
@@ -1,36 +1,64 @@
-<?php get_header(); ?>
+<?php
+$css_escolhido = 'single';
+require_once('header.php');
+?>
 
 <main>
-<article> 
-<?php if(have_posts()){ ?>
+	
+	<article>
 
-   <?php while(have_posts()){
-        the_post(); ?>
-        
-            <div class="single-imovel-thumbnail">
-                <?php the_post_thumbnail(); ?>
-            </div>
+		<?php if( have_posts() ) {
+			while( have_posts() ) {
+				the_post(); ?>
 
-            <div class="container">
-                <section class="chamada-principal">
-                    <?php the_title(); ?> 
-                </section>
+		<div class="single-imovel-thumbnail">
+			<?php the_post_thumbnail(); ?>
+		</div>
 
-                <section class="single-imovel-geral">
-                    <div class="single-imovel-descricao">
-                        <?php the_content(); ?> 
-                    </div>
+		<div class="container">
+			<section class="chamada-principal">
+				<h1><?php the_title(); ?></h1>
+			</section>
 
-                </section>
-                <span class="single-imovel-data">
-                    <?php the_date(); ?>
-                </span>
-            </div>
-       
+			<section class="single-imovel-geral">
+				
+				<div class="single-imovel-descricao">
+					<?php the_content(); ?>
+				</div>
 
-    <?php }//while
+				<?php	$imoveis_meta_data = get_post_meta( $post->ID ); ?>
+				 
+				<dl class="single-imovel-informacoes">
+					<dt>Preço</dt>
+					<dd>R$ <?= esc_attr( $imoveis_meta_data['preco_id'][0] ); ?></dd>
 
-         }//if ?>
-</article>
+					<dt>Vagas</dt>
+					<dd><?= esc_attr( $imoveis_meta_data['vagas_id'][0] ); ?></dd>
+
+					<dt>Banheiros</dt>
+					<dd><?= esc_attr( $imoveis_meta_data['banheiros_id'][0] ); ?></dd>
+					
+					<dt>Quartos</dt>
+					<dd><?= esc_attr( $imoveis_meta_data['quartos_id'][0] ); ?></dd>
+				</dl>
+
+
+			</section>
+
+			<span class="single-imovel-data">
+			 <?php the_date(); ?>
+			</span>
+
+
+		</div>
+		
+		<?php }
+		} ?>
+
+	</article>
+
 </main>
+
+
+
 <?php get_footer(); ?>
